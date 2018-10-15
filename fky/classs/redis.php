2e4fbea1e70ce5f2e8cc3650082827fc547a56bb
mingdiantianxia <1982104592@qq.com>
2018-10-15 19:20:09 +0800

    注释

--- fky/classs/redis.php
+++ fky/classs/redis.php
@@ -3,6 +3,13 @@ namespace fky\classs;
 require __DIR__.'/../inc/predis/autoload.php';
 use Predis;
 class Redis extends Predis\Client{
+
+    /**
+     * 集群设置
+     * Redis constructor.
+     * @param array $parameters  ['tcp://127.0.0.1:6379',];
+     * @param null $options		['cluster' => 'redis','parameters' => ['password' => '集群的统一密码']];
+     */
 	function __construct($parameters = array('host' => '127.0.0.1', 'port' => 6379, 'database' => 15), $options = null){
 		parent::__construct($parameters, $options);
 	}
