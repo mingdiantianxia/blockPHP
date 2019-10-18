9f6f175526cda74c4250084b97b9df3ae9b83b18
mingdiantianxia <1982104592@qq.com>
2019-10-18 14:21:41 +0800

    docker test

---
+++ config/mqtt.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ *  mqtt环境配置
+ */
+return [
+    'mqtt' => [
+        //mqtt服务器地址
+        'host'  => '',
+        //mqtt端口
+        'port'  => '1883',
+        //mqtt客户端id
+        'client_id' => '',
+        //mqtt账户名
+        'username' => '',
+        //mqtt密码
+        'password' => '',
+    ],
+];
---
+++ fky/cli/docker/install_docker_.sh
@@ -0,0 +1,20 @@
+#/bin/bash
+# step 1: 安装必要的一些系统工具
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+
+# Step 2: 添加软件源信息
+sudo yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+
+# Step 3: 更新并安装Docker-CE
+sudo yum makecache fast
+sudo yum -y install docker-ce docker-ce-cli containerd.io
+
+# Step 4: 开启Docker服务
+#sudo service docker start
+
+# docker镜像加速
+# 在dockerd后面加参数
+echo "ExecStart=/usr/bin/dockerd --registry-mirror=https://registry.docker-cn.com" >> /usr/lib/systemd/system/docker.service
+
+# 重启docker
+sudo service docker restart
